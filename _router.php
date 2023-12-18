d8696e18006df36b2365452d8945664035651449
Mateus <atrujillomateus@gmail.com>
2023-12-18 00:36:02 -0500

    You might need the vars.

--- _router.php
+++ _router.php
@@ -1,4 +1,5 @@
 <?php
+require_once "./_var.php";
 require_once $TO_HOME . "_functions.php";
 $url = $_GET["uri"] ?? "/";
 while (strlen($url) > 0 && substr($url, 0, 1) != "/") $url = substr($url, 1);
