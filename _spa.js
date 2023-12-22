edb6bc21d61b8b990c39ef8f8b2e606384a0c939
Mateus <atrujillomateus@gmail.com>
2023-12-22 16:13:49 -0500

    Improve functions to support component update.

--- _spa.js
+++ _spa.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
         _GET = JSON.parse(localStorage.getItem("_GET")),
         _POST = JSON.parse(localStorage.getItem("_POST")),
         HISTORY_INDEX = -1,
-        HISTORY_PATH = [];
+        HISTORY_PATH = [],
+        COMPONENTS = {};
     const ROUTES = JSON.parse(localStorage.getItem("ROUTES")),
         TO_HOME = localStorage.getItem("TO_HOME"),
         HOME_PATH = localStorage.getItem("HOME_PATH");
@@ -25,31 +26,32 @@ document.addEventListener('DOMContentLoaded', function () {
             data: { custom_error_message },
             success: function (data) {
                 document.write(data);
+                $("head").append(`<script src='${HOME_PATH}_spa.js' defer></script>
+                <script>
+                const parseURL = ${parseURL}, routeURL = ${routeURL}, loadSPA = ${loadSPA}, getLocalStorageItems = ${getLocalStorageItems}, ROUTES = ${JSON.stringify(ROUTES)}, HOME_PATH = "${HOME_PATH}";
+                let HISTORY_PATH = ${JSON.stringify(HISTORY_PATH)}; HISTORY_INDEX = ${HISTORY_INDEX}, _GET = ${JSON.stringify(_GET)}, _POST = ${JSON.stringify(_POST)}, COMPONENTS = ${JSON.stringify(COMPONENTS)};
+                window.addEventListener("popstate", function (e) {
+                    HISTORY_INDEX = e.state.index;
+                    loadSPA(HISTORY_PATH[HISTORY_INDEX], false);
+                });
+                </script>`);
             }, error: function (xhr, status, error) {
                 console.log("Error loading content:", error);
             }
         });
     };
-    const loadHeaderFooter = function () {
+    const reloadComponent = function (file, component) {
+        COMPONENTS[component] = file;
         $.ajax({
             async: false,
-            url: `${HOME_PATH}header.php`,
+            url: `${HOME_PATH}${file}`,
             success: function (data) {
-                $("#header").html(data);
+                $(component).html(data);
             }, error: function (xhr, status, error) {
                 console.log("Error loading content:", error);
             }
         });
-        $.ajax({
-            async: false,
-            url: `${HOME_PATH}footer.php`,
-            success: function (data) {
-                $("#footer").html(data);
-            }, error: function (xhr, status, error) {
-                console.log("Error loading content:", error);
-            }
-        });
-    }
+    };
     const parseURL = function (uri = "/") {
         while (uri.length > 0 && !uri.startsWith("/")) uri = uri.substring(1);
         while (uri.length > 1 && uri.endsWith("/")) uri = uri.substring(0, uri.length - 1);
@@ -64,30 +66,31 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     const routeURL = function (uri = "/") {
         const { path, params } = parseURL(uri);
-        if (!ROUTES.hasOwnProperty(path) || !Object.entries(ROUTES).length || (!ROUTES[path].GET && !ROUTES[path].POST))
+        if (!ROUTES.hasOwnProperty(path) || !Object.entries(ROUTES).length || (!ROUTES[path]?.GET && !ROUTES[path]?.POST))
             errorPage(404, `Route "${uri}" does not exist.`);
         localStorage.setItem("URI", path);
-        localStorage.setItem("_GET", JSON.stringify({ ..._GET, ...ROUTES[path].GET ?? [], ...params }));
-        localStorage.setItem("_POST", JSON.stringify({ ..._POST, ...ROUTES[path].POST ?? [] }));
+        localStorage.setItem("_GET", JSON.stringify({ ..._GET, ...ROUTES[path]?.GET ?? [], ...params }));
+        localStorage.setItem("_POST", JSON.stringify({ ..._POST, ...ROUTES[path]?.POST ?? [] }));
         getLocalStorageItems();
-        uri = ROUTES[path].URI;
-        if (uri == "") uri = _GET["uri"] ? (ROUTES[_GET["uri"]].URI ? ROUTES[_GET["uri"]].URI : ROUTES["/"].URI) : ROUTES["/"].URI;
+        uri = ROUTES[path]?.URI;
+        if (uri == "") uri = _GET["uri"] ? (ROUTES[_GET["uri"]]?.URI ? ROUTES[_GET["uri"]]?.URI : ROUTES["/"]?.URI) : ROUTES["/"]?.URI;
         else _GET["uri"] = URI;
-        return { path, uri, file: ROUTES[path].FILE, get: _GET, post: _POST };
+        return { path, uri, file: ROUTES[path]?.FILE, get: _GET, post: _POST, component: ROUTES[path]?.COMPONENT };
     };
     const loadSPA = function (url, push = true) {
         $(".load-circle-back, .load-circle-fore, .load-text, .loading").fadeIn(1);
         $("#spa-page-content-container").html("");
-        const { path, uri, file, get, post } = routeURL(`${url}`);
-        console.log(`loadSPA("${url}");`);
-        console.log("routeURL(); PATH=", path, "; URI=", uri, "; FILE=", file, "; _GET=", get, "; _POST=", post);
+        const { path, uri, file, get, post, component } = routeURL(`${url}`);
+        /* console.log(`loadSPA("${url}");`);
+        console.log("routeURL(); PATH=", path, "; URI=", uri, "; FILE=", file, "; _GET=", get, "; _POST=", post, "; COMPONENT=", component); */
         if (push) historyPushState(url);
+        if (!$("#spa-page-content-container").length) location.reload();
         if (!file) $.ajax({
             type: "POST",
             url: `${HOME_PATH}${uri}?${new URLSearchParams(get).toString()}`,
             data: { ...post },
             success: function (data) {
-                if (uri != ROUTES[path].URI) loadHeaderFooter();
+                for (let key in component) if (component[key] != COMPONENTS[key]) reloadComponent(component[key], key);
                 $("#spa-page-content-container").html(data);
             }, error: function (xhr, status, error) {
                 console.log("Error loading content:", error);
@@ -107,7 +110,6 @@ document.addEventListener('DOMContentLoaded', function () {
         loadSPA($(this).attr("href"));
     });
     $(document).ready(function () {
-        loadHeaderFooter();
         loadSPA(`${TO_HOME == "./" ? "." : TO_HOME}${URI}`);
     });
 });
