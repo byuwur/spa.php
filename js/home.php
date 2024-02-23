49243a3e6a2b07674d4abdcc7343b2e55bc1bd99
Mateus <atrujillomateus@gmail.com>
2024-02-23 08:41:35 -0500

    Folder rejection script optimal

--- js/home.php
+++ js/home.php
@@ -1,13 +1,3 @@
-<?php require_once "../_var.php"; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>No</title>
-</head>
-<body>
-    Sorry, you shouldn't be here.
-</body>
-</html>
-<?php header("Location:" . $TO_HOME); ?>
+<?php require_once "../_var.php";
+header("Location: ${HOME_PATH}");
+exit;
