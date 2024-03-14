b61a4ec252ae8faeeca8bdbd8775c57fb862db36
Mateus <atrujillomateus@gmail.com>
2024-03-13 22:50:42 -0500

    Fix deprecated PHP string curled variables.

--- _routes.php
+++ _routes.php
@@ -7,13 +7,13 @@ $ROOT_COMPONENTS = ["COMPONENT" => ["#header" => "/header.php", "#footer" => "/f
 $routes = [
     // "/uri" => ["URI" => "/file.php", "GET" => [..."key" => "value"...], "POST" => [..."key" => "value"...], "COMPONENT" => [..."#id" => "file.php"...]],
     // "/" routes
-    "${ROOT_ROUTE}" => ["URI" => "/main.php", ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}es" => ["URI" => "", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}en" => ["URI" => "", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}inicio" => ["URI" => "/main.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}home" => ["URI" => "/main.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}pagina1" => ["URI" => "/page1.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}page1" => ["URI" => "/page1.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}pagina2" => ["URI" => "/page2.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
-    "${ROOT_ROUTE}page2" => ["URI" => "/page2.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE => ["URI" => "/main.php", ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "es" => ["URI" => "", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "en" => ["URI" => "", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "inicio" => ["URI" => "/main.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "home" => ["URI" => "/main.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "pagina1" => ["URI" => "/page1.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "page1" => ["URI" => "/page1.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "pagina2" => ["URI" => "/page2.php", "GET" => ["lang" => "es"], ...$ROOT_COMPONENTS],
+    $ROOT_ROUTE . "page2" => ["URI" => "/page2.php", "GET" => ["lang" => "en"], ...$ROOT_COMPONENTS],
 ];
--- js/home.php
+++ js/home.php
@@ -1,3 +1,3 @@
 <?php require_once "../_var.php";
-header("Location: ${HOME_PATH}");
+header("Location: " . $HOME_PATH);
 exit;
