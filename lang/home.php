50e371d785e6114bed7c5953fb9a84a0dcc4580b
Mateus <atrujillomateus@gmail.com>
2024-08-28 14:50:38 -0500

    To HOME_PATH when invalid

--- lang/home.php
+++ lang/home.php
@@ -1,3 +1,4 @@
-<?php require_once "../_var.php";
-header("Location: " . $HOME_PATH);
-exit;
+<?php
+require_once "../_var.php";
+require_once $TO_HOME . "_functions.php";
+change_location($HOME_PATH);
