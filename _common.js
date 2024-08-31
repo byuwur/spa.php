10ef208dee91bf58b8745af7d47973eda0cfbe4a
Mateus <atrujillomateus@gmail.com>
2024-08-30 19:21:39 -0500

    Improve <Sidebar /> handling.

--- _common.js
+++ _common.js
@@ -6,6 +6,106 @@
  * Copyright (c) 2023 Andrés Trujillo [Mateus] byUwUr
  */
 
+// Common selectors
+const SIDEBAR_ID = "#sidebar",
+	SIDERBAR_TOGGLE_ID = "#sidebar-toggle",
+	SIDEBAR_HIDDEN_ID = "#sidebar-hidden",
+	APP_CONTAINER_SELECTOR = ".app-container";
+
+/**
+ * Initializes the <Sidebar /> component in #spa-nav.
+ */
+function initSidebar() {
+	// Check it exists in the first place. Duh..
+	const jqSidebar = $(SIDEBAR_ID);
+	if (!jqSidebar.length) return;
+	console.log("Init <Sidebar />");
+	if (!get_cookie("SidebarExpand")) set_cookie("SidebarExpand", "on");
+	// Init the rest of the elements
+	const jqSidebarToggle = $(SIDERBAR_TOGGLE_ID);
+	const jqSidebarHidden = $(SIDEBAR_HIDDEN_ID);
+	const jqAppContainer = $(APP_CONTAINER_SELECTOR);
+	// Ensure the overlay inside the sidebar follows it accordingly, due to being an absolute positioned inside another
+	jqSidebar
+		.off("scroll")
+		.on(
+			"scroll",
+			(function () {
+				let sidebarScrollTop = 0;
+				return debounce(function () {
+					const top = Math.floor($(this).scrollTop()),
+						diff = top - sidebarScrollTop;
+					console.log(top);
+					if (jqSidebar.hasClass("sidebar-expanded")) $(`${SIDEBAR_ID} .overlay`).css("height", `${$(`${SIDEBAR_ID} .overlay`).height() + diff}px`);
+					sidebarScrollTop = top;
+				});
+			})()
+		)
+		// Ensure the sidebar collapses when the mouse leaves the sidebar itself
+		.off("mouseleave")
+		.on("mouseleave", function () {
+			if (!jqSidebarToggle.hasClass("sidebar-expanded")) jqSidebar.removeClass("sidebar-expanded");
+		});
+	// Toggle sidebar expansion when the sidebar toggle button is clicked
+	jqSidebarToggle.off("click").on("click", function () {
+		jqSidebarToggle.trigger("blur");
+		$("#sidebar .overlay").css("height", "");
+		if (!jqSidebarToggle.hasClass("sidebar-expanded")) {
+			jqSidebarToggle.addClass("sidebar-expanded");
+			jqSidebar.addClass("sidebar-expanded");
+			jqAppContainer.addClass("sidebar-expanded");
+			jqSidebarHidden.css("display", "none");
+			set_cookie("SidebarExpand", "on");
+		} else {
+			jqSidebarToggle.removeClass("sidebar-expanded");
+			jqSidebar.removeClass("sidebar-expanded");
+			jqAppContainer.removeClass("sidebar-expanded");
+			jqSidebarHidden.css("display", "flex");
+			jqSidebar.scrollTop(0);
+			set_cookie("SidebarExpand", "off");
+		}
+	});
+	// Expand sidebar when the hidden sidebar area is hovered
+	jqSidebarHidden.off("mouseenter").on("mouseenter", function () {
+		if (!jqSidebarToggle.hasClass("sidebar-expanded")) jqSidebar.addClass("sidebar-expanded");
+	});
+	// Collapse sidebar when the mouse leaves the hidden sidebar area
+	jqSidebarHidden.off("mouseleave").on("mouseleave", function () {
+		if (!jqSidebarToggle.hasClass("sidebar-expanded") && !jqSidebar.is(":hover")) jqSidebar.removeClass("sidebar-expanded");
+	});
+	// Expand the sidebar automatically on larger screens (min-width: 768px)
+	if (window.matchMedia("(min-width: 768px)").matches && get_cookie("SidebarExpand") == "on") {
+		jqSidebarToggle.addClass("sidebar-expanded");
+		jqSidebar.addClass("sidebar-expanded");
+		jqAppContainer.addClass("sidebar-expanded");
+	}
+}
+
+/**
+ * Some other initializations for common resources in the page.
+ */
+function initMisc() {
+	// Smooth scroll for links with hashes in their href (excluding empty hashes)
+	$("a[href*='#']:not([href='#'])")
+		.off("click")
+		.on("click", function (event) {
+			event.preventDefault();
+			// Scroll to the target element if it exists on the same page
+			if ($(this.hash).length && location.pathname == this.pathname && location.hostname == this.hostname) $(`html, body, ${APP_CONTAINER_SELECTOR}`).animate({ scrollTop: $(this.hash).offset().top - 0 }, 999, "easeInOutExpo");
+			// Collapse the navbar after clicking the link
+			setTimeout(() => {
+				$(".navbar-collapse").collapse("hide");
+				if (window.matchMedia("(max-width: 768px)").matches && $(SIDERBAR_TOGGLE_ID).hasClass("sidebar-expanded")) {
+					$(SIDERBAR_TOGGLE_ID).removeClass("sidebar-expanded");
+					$(SIDEBAR_ID).removeClass("sidebar-expanded");
+					$(APP_CONTAINER_SELECTOR).removeClass("sidebar-expanded");
+					$(SIDEBAR_HIDDEN_ID).css("display", "flex");
+					$(SIDEBAR_ID).scrollTop(0);
+				}
+			}, 333);
+		});
+}
+
 /**
  * Initializes all Bootstrap components within the #spa-content.
  * It should be called whenever the content of the #spa-page-content-container changes dynamically to ensure that all components function correctly.
@@ -50,103 +150,6 @@ function initBootstrapComponents() {
 }
 
 /**
- * Initializes the <Sidebar /> component in #spa-nav.
- */
-function initSidebar() {
-	// Check it exists in the first place. Duh..
-	if (!$("#sidebar").length) return;
-	console.log("Init <Sidebar />");
-	if (!get_cookie("SidebarExpand")) set_cookie("SidebarExpand", "on");
-	// Ensure the overlay inside the sidebar follows it accordingly, due to being an absolute positioned inside another
-	$("#sidebar")
-		.off("scroll")
-		.on(
-			"scroll",
-			(function () {
-				let sidebarScrollTop = 0;
-				return debounce(function () {
-					const top = Math.floor($(this).scrollTop()),
-						diff = top - sidebarScrollTop;
-					console.log(top);
-					if ($("#sidebar").hasClass("sidebar-expanded")) $("#sidebar .overlay").css("height", `${$("#sidebar .overlay").height() + diff}px`);
-					sidebarScrollTop = top;
-				});
-			})()
-		)
-		// Ensure the sidebar collapses when the mouse leaves the sidebar itself
-		.off("mouseleave")
-		.on("mouseleave", function () {
-			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").removeClass("sidebar-expanded");
-		});
-	// Toggle sidebar expansion when the sidebar toggle button is clicked
-	$("#sidebar-toggle")
-		.off("click")
-		.on("click", function () {
-			$("#sidebar-toggle").trigger("blur");
-			$("#sidebar .overlay").css("height", "");
-			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) {
-				$("#sidebar-toggle").addClass("sidebar-expanded");
-				$("#sidebar").addClass("sidebar-expanded");
-				$(".app-container").addClass("sidebar-expanded");
-				$("#sidebar-hidden").css("display", "none");
-				set_cookie("SidebarExpand", "on");
-			} else {
-				$("#sidebar-toggle").removeClass("sidebar-expanded");
-				$("#sidebar").removeClass("sidebar-expanded");
-				$(".app-container").removeClass("sidebar-expanded");
-				$("#sidebar-hidden").css("display", "flex");
-				$("#sidebar").scrollTop(0);
-				set_cookie("SidebarExpand", "off");
-			}
-		});
-	// Expand sidebar when the hidden sidebar area is hovered
-	$("#sidebar-hidden")
-		.off("mouseenter")
-		.on("mouseenter", function () {
-			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").addClass("sidebar-expanded");
-		});
-	// Collapse sidebar when the mouse leaves the hidden sidebar area
-	$("#sidebar-hidden")
-		.off("mouseleave")
-		.on("mouseleave", function () {
-			if (!$("#sidebar-toggle").hasClass("sidebar-expanded") && !$("#sidebar").is(":hover")) $("#sidebar").removeClass("sidebar-expanded");
-		});
-	// Expand the sidebar automatically on larger screens (min-width: 768px)
-	if (window.matchMedia("(min-width: 768px)").matches && get_cookie("SidebarExpand") == "on") {
-		$("#sidebar-toggle").addClass("sidebar-expanded");
-		$("#sidebar").addClass("sidebar-expanded");
-		$(".app-container").addClass("sidebar-expanded");
-	}
-}
-
-/**
- * Some other initializations for common resources in the page.
- */
-function initMisc() {
-	// Smooth scroll for links with hashes in their href (excluding empty hashes)
-	$("a[href*='#']:not([href='#'])")
-		.off("click")
-		.on("click", function (event) {
-			event.preventDefault();
-			// Scroll to the target element if it exists on the same page
-			if ($(this.hash).length && location.pathname == this.pathname && location.hostname == this.hostname) {
-				$("html, body").animate({ scrollTop: $(this.hash).offset().top - 0 }, 999, "easeInOutExpo");
-			}
-			// Collapse the navbar after clicking the link
-			setTimeout(() => {
-				$(".navbar-collapse").collapse("hide");
-				if (window.matchMedia("(max-width: 768px)").matches && $("#sidebar-toggle").hasClass("sidebar-expanded")) {
-					$("#sidebar-toggle").removeClass("sidebar-expanded");
-					$("#sidebar").removeClass("sidebar-expanded");
-					$(".app-container").removeClass("sidebar-expanded");
-					$("#sidebar-hidden").css("display", "flex");
-					$("#sidebar").scrollTop(0);
-				}
-			}, 333);
-		});
-}
-
-/**
  * Reloads Google ReCaptcha if present
  */
 function initReCaptcha() {
@@ -166,8 +169,8 @@ function initReCaptcha() {
 function initCommon() {
 	if (typeof jQuery === "undefined" && window.jQuery === undefined) return console.error("Init _common.js FAILED. No jQuery found.");
 	console.log("Init _common.js");
-	initBootstrapComponents();
 	initSidebar();
-	initMisc();
+	initBootstrapComponents();
 	initReCaptcha();
+	initMisc();
 }
