190aa45d94dc511e9785f7b59187c07be5ce2a5e
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-02-20 03:57:17 -0500

    Improve <Sidebar /> visuals.

--- sidebar.php
+++ sidebar.php
@@ -14,7 +14,7 @@ require_once $TO_HOME . "_routes.php";
     <div class="bywr-sidebar-content accordion-item flex-grow-1">
         <div class="bywr-sidebar-option p-2o5">
             <div class="navbar-brand has-background-contain" style="height:48px;width:48px;background-image:url('<?= $HOME_PATH; ?>/img/byuwur.png');"></div>
-            <span class="ms-2 me-5 pe-5">easy-SPA-PHP</span>
+            <span class="ms-2 me-4 pe-5">easy-SPA-PHP</span>
         </div>
         <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE . $_home; ?>">
             <i class="fas fa-home"></i><span><?= $home; ?></span><i class="fas fa-angle-right ms-auto"></i>
