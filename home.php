222b482eb40d4fbb0e176a63a020a25461b58c4c
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-03-03 17:14:06 -0500

    Swiper.JS

--- home.php
+++ home.php
@@ -34,9 +34,11 @@ require_once $TO_HOME . "_router.php";
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Easy PHP SPA | byUwUr</title>
     <link rel="icon" type="image/png" href="<?= $HOME_PATH; ?>/img/byuwur.png" />
+    <link rel="stylesheet" href="<?= $HOME_PATH; ?>/css/swiper.min.css" />
     <link rel="stylesheet" href="<?= $HOME_PATH; ?>/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= $HOME_PATH; ?>/css/fontawesome.min.css" />
     <link rel="stylesheet" href="<?= $HOME_PATH; ?>/_common.css" />
+    <script src="<?= $HOME_PATH; ?>/js/swiper.min.js" defer></script>
     <script src="<?= $HOME_PATH; ?>/js/jquery.min.js" defer></script>
     <script src="<?= $HOME_PATH; ?>/js/popper.min.js" defer></script>
     <script src="<?= $HOME_PATH; ?>/js/bootstrap.min.js" defer></script>
