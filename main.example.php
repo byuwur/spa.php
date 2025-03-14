4aa44b62279cc7179ea9034be5af433855107b2d
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-03-13 19:36:50 -0500

    Implement video with subtitles.

--- main.example.php
+++ main.example.php
@@ -12,16 +12,47 @@ require_once $TO_HOME . "_common.php";
 require_once $TO_HOME . "common.php";
 ?>
 <div class="video-foreground app-container">
-    <div class="container vh-100 d-flex flex-column align-items-center justify-content-center text-white text-dark-shadow">
-        <p><?= $description; ?></p>
-        <p><?= $thisis . " " . $home; ?>.</p>
-        <p><?= $home . ": "; ?><b><i>"<?= $home_desc; ?>"</i></b></p>
+    <div class="container vh-100 d-flex flex-column align-items-center justify-content-center">
+        <div class="text-white text-dark-shadow">
+            <p><?= $description; ?></p>
+            <p><?= $thisis . " " . $home; ?>.</p>
+            <p><?= $home . ": "; ?><b><i>"<?= $home_desc; ?>"</i></b></p>
+            <hr class="w-100" />
+            <p>Video Demo:</p>
+        </div>
+        <video id="subtitle-sample" class="video-js w-100" poster="<?= $HOME_PATH; ?>/img/video/sample.jpg" controls playsinline>
+            <source src="<?= $HOME_PATH; ?>/img/video/sample.mp4" type="video/mp4" />
+            <track src="<?= $HOME_PATH; ?>/img/video/sample.en.vtt" kind="captions" srclang="en" label="English" default />
+            <track src="<?= $HOME_PATH; ?>/img/video/sample.es.vtt" kind="captions" srclang="es" label="Español" />
+        </video>
     </div>
 </div>
 <script>
     $(() => {
         document.title = "<?= $titles[$title_index]; ?>";
         byCommon.init();
+
+        const byVideoPlayer = videojs("subtitle-sample", {
+            controls: true,
+            autoplay: false,
+            preload: 'auto',
+            fluid: true,
+            playbackRates: [0.5, 1, 1.5, 2],
+            controlBar: {
+                currentTimeDisplay: true,
+                timeDivider: true,
+                durationDisplay: true,
+                remainingTimeDisplay: true,
+                progressControl: true,
+                fullscreenToggle: true,
+                pictureInPictureToggle: true,
+                playbackRateMenuButton: true,
+                chaptersButton: true,
+                volumePanel: {
+                    inline: false
+                },
+            }
+        });
     });
 </script>
 <?php
