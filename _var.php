eb021175e442033a899b56ae2c0ee349a20018eb
Mateus <atrujillomateus@gmail.com>
2025-10-28 16:25:35 -0500

    [Upgrade] Normalize string. Account for CLI executions.

--- _var.php
+++ _var.php
@@ -8,18 +8,23 @@
 
 // Initializes the output buffer and sets up paths and environment variables.
 ob_start();
+$INVOKER__FILE__ = isset($INVOKER__FILE__) ? str_replace("\\", "/", $INVOKER__FILE__) : "";
+$THIS__FILE__ = str_replace("\\", "/", __FILE__);
+$SERVER_SCRIPT_FILENAME = str_replace("\\", "/", $_SERVER["SCRIPT_FILENAME"]);
+$SERVER_PHP_SELF = str_replace("\\", "/", $_SERVER["PHP_SELF"]);
+$IS_PHP_ON_SERVER = php_sapi_name() != 'cli';
 // Set the root directory of the system
-$SYSTEM_ROOT = dirname(str_replace("\\", "/", __FILE__));
+$SYSTEM_ROOT = dirname($THIS__FILE__);
 // Determine the protocol (HTTP or HTTPS)
 $PROTOCOL = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ? "https://" : "http://";
 // Calculate the difference in directory depth between the current script and the root directory
-$PATH_DIFF = count(explode("/", str_replace("\\", "/", $_SERVER["SCRIPT_FILENAME"]))) - count(explode("/", str_replace("\\", "/", __FILE__)));
+$PATH_DIFF = count(explode("/", ($IS_PHP_ON_SERVER ? $_SERVER["SCRIPT_FILENAME"] : $INVOKER__FILE__))) - count(explode("/", $THIS__FILE__));
 // Set the relative path to the home directory
-$TO_HOME = $PATH_DIFF ? str_repeat("../", $PATH_DIFF) : "./";
+$TO_HOME = $PATH_DIFF > 0 ? str_repeat("../", $PATH_DIFF) : "./";
 // Get the current script's directory path
-$THIS_PATH = str_replace("\\", "/", dirname($PROTOCOL . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"]));
+$THIS_PATH = $IS_PHP_ON_SERVER ? dirname($PROTOCOL . $_SERVER["HTTP_HOST"] . $SERVER_PHP_SELF) : dirname($INVOKER__FILE__);
 // Set the absolute path to the home directory
-$HOME_PATH = $PATH_DIFF ? implode("/", array_slice(explode("/", $THIS_PATH), 0, -$PATH_DIFF)) : $THIS_PATH;
+$HOME_PATH = $PATH_DIFF > 0 ? implode("/", array_slice(explode("/", $THIS_PATH), 0, -$PATH_DIFF)) : $THIS_PATH;
 // Store the calculated paths in the browser's localStorage
 if (isset($setLocalStorage) && $setLocalStorage) { ?>
     <script>
