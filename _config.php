a0b8a1af4e36afd82075d2f7b5f975065aa7290a
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2026-05-04 06:10:49 -0500

    Safer mysqli unicode handling. Strict mysqli error reporting.

--- _config.php
+++ _config.php
@@ -7,7 +7,9 @@
  */
 
 try {
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $mysqli = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"], $_ENV["DB_PORT"]);
-} catch (Exception $e) {
-    api_respond(500, true, "Connection failed" . ($_ENV["APP_ENV"] === "DEV" ? ": {$e->getCode()} = {$e->getMessage()}" : ""));
+    $mysqli->set_charset("utf8mb4");
+} catch (Throwable $e) {
+    api_respond(500, true, "Connection failed" . (($_ENV["APP_ENV"] ?? "PROD") === "DEV" ? ": {$e->getCode()} = {$e->getMessage()}" : ""));
 }
