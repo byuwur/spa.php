de1c0381851e2369fb8c1d220e04621c69df61ed
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2026-08-13 23:57:31 -0500

    [Hotfix] Swing animation between sections.

--- _common.js
+++ _common.js
@@ -99,7 +99,7 @@
         if ($(this.hash).length)
           $(`html, body, ${byCommon.APP_CONTAINER_SELECTOR}`)
             .stop()
-            .animate({ scrollTop: $(this.hash).offset().top - 0 }, 999, "easeInOutExpo");
+            .animate({ scrollTop: $(this.hash).offset().top - 0 }, 99, "swing");
         // Collapse the navbar after clicking the link
         setTimeout(() => {
           $(".navbar-collapse").collapse("hide");
